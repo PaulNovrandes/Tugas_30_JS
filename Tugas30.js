30e405ae37531d58c0147388659f105ed37f7b47
PaulNovrandes <novrandes.tanuma@gmail.com>
2023-06-02 09:23:29 +0700

    Tugas 30 JS
    Tugas mencari carakter spesifik. Tugas ini dikumpulkan kepada niomic.

---
+++ Tugas30.js
@@ -0,0 +1,7 @@
+function panggilCharSpec(a){
+  var getData = a.match(/[es]/g);
+
+  console.log(getData);
+}
+
+panggilCharSpec("Para tetua adat mendiskusikan masalah kejahantan yang terjadi di rumah adat");
